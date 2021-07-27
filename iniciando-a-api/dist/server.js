8c0e443f967fd47c79101b55dc8f3c261ae1e65b
Tamires Santos <tfs.tamsantos@gmail.com>
2021-07-27 15:14:27 -0300

    C2 - Adicionando os tipos

--- iniciando-a-api/dist/server.js
+++ iniciando-a-api/dist/server.js
@@ -4,8 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = require("./routes");
 var app = express_1.default();
-app.get('/', function (request, response) {
-    return response.json({ message: 'Hello World!' });
-});
+app.get('/', routes_1.createCourse);
 app.listen(3333);
---
+++ iniciando-a-api/src/CreateCourseService.ts
@@ -0,0 +1,13 @@
+interface Course {
+  name: string;
+  duration: number;
+  educator: string;
+}
+
+class CreateCourseService {
+  execute({ duration, educator, name }: Course) {
+    console.log(name, duration, educator)
+  }
+}
+
+export default new CreateCourseService()
